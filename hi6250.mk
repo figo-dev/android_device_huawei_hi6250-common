23446de817e0967c17872288391a1da150b0b0f6
Haky86 <hakamelassouad@gmail.com>
2021-07-13 17:03:50 +0200

    hi6205-common: Build camera stack
    Signed-off-by: Haky86 <hakamelassouad@gmail.com>
    Change-Id: I57866672ec622a981583d2e1600f623a8977e3e7

--- hi6250.mk
+++ hi6250.mk
@@ -46,6 +46,11 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl \
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
