e58d20f5f653d4cbd21e56ca78d80a95a4001955
Haky86 <hakamelassouad@gmail.com>
2021-07-13 17:36:57 +0200

    hi6250-common: Build Lights stack
    Signed-off-by: Haky86 <hakamelassouad@gmail.com>
    Change-Id: I267866381b3d89b0389cb6beb3967046cde2931d

--- hi6250.mk
+++ hi6250.mk
@@ -109,6 +109,11 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service \
+
 # Recovery
 AB_OTA_UPDATER := false
 
