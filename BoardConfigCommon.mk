bfabdb2da158fe2e4bd4c9cc3877af1f73806d89
Huawei-Dev <surdu_petru@yahoo.com>
2021-07-13 14:56:54 +0200

    hi6250-common: Setup arch
    Change-Id: I3a7d9c181cb2670440a4e3608873e80ffe361ebf

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -4,6 +4,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+BOARD_VENDOR := huawei
+
 COMMON_PATH := device/huawei/hi6250-common
+
+# 64 Bit
+TARGET_USES_64_BIT_BINDER := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
 
 include vendor/huawei/hi6250-common/BoardConfigVendor.mk
